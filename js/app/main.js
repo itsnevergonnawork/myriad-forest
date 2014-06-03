d357aacbeee0026053a180bb75b4ba9bf81032e2
Michał Koziarski <michalkoziarski@fastmail.fm>
2014-06-04 00:34:14 +0200

    Implement sliding page navigation

--- js/app/main.js
+++ js/app/main.js
@@ -1,2 +1,8 @@
-define(function() {
+define([
+    'domReady',
+    'app/navigation'
+], function(domReady, navigation) {
+    domReady(function() {
+        navigation.init();
+    });
 });
---
+++ js/app/navigation.js
@@ -0,0 +1,50 @@
+define([
+    'jquery',
+    'jquery.pjax'
+], function($, pjax) {
+    var tray, incoming;
+
+    var initDom = function initDom() {
+        tray = $('#tray');
+        incoming = $('.content').clone();
+        incoming.attr('class', 'incoming');
+        tray.append(incoming);
+    };
+
+    var init = function init() {
+        initDom();
+
+        $('.incoming').on('pjax:end', function(event) {
+            var incoming = $(this),
+                newPage = incoming.clone(),
+                prevRight = $('.content.right');
+
+            newPage.removeClass('incoming');
+            newPage.addClass('content');
+            newPage.addClass('right');
+            console.log(prevRight);
+            prevRight.remove();
+            tray.append(newPage);
+            tray.on('transitionend', function() {
+                tray.find('.content').not('.left, .right').remove();
+                tray.removeClass('anim');
+                tray.removeClass('left');
+                newPage.removeClass('right');
+            })
+            tray.addClass('anim');
+            tray.addClass('left');
+        });
+        if ($.support.pjax) {
+            $(document).on('click', 'a', function(event) {
+                $.pjax.click(event, {
+                    container: '.incoming',
+                    fragment: '.content'
+                });
+            });
+        }
+    };
+
+    return {
+        init: init
+    };
+});
