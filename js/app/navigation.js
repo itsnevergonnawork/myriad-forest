266fab2285c4cb4b696159c79ef382718bd65452
Michał Koziarski <michalkoziarski@fastmail.fm>
2014-06-06 12:54:08 +0200

    Fix page transitions, implement sliding to the right
    Fixed (?) the weird transition effect by waiting 30 ms between expanding
    the tray and starting the animation.

--- js/app/navigation.js
+++ js/app/navigation.js
@@ -17,7 +17,8 @@ define([
         $('.incoming').on('pjax:end', function(event) {
             var incoming = $(this),
                 newPage = incoming.find('.content').clone(),
-                oldPage = tray.children('.content');
+                oldPage = tray.children('.content'),
+                moveRight = true;
 
             var completeTrans = function() {
                 tray.removeClass('anim');
@@ -27,20 +28,28 @@ define([
                 oldPage.remove();
 
                 // contract tray, new page stays in view
-                tray.removeClass('expanded');
+                tray.removeClass('expanded left flip');
 
                 // revealing the new page done
                 newPage.removeClass('new');
+                tray.off('transitionend', completeTrans);
             };
 
             newPage.addClass('new');
             //prevRight.remove();
-            tray.addClass('expanded');
+            tray.addClass('expanded' + (moveRight ? ' left' : ''));
             tray.append(newPage);
 
             tray.on('transitionend', completeTrans);
-            tray.addClass('anim');
-            tray.addClass('slide');
+
+            // adding the anim class right after the other style changes
+            // and/or adding the new content seems to be animating in the middle
+            // of those previous changes, or something.
+            // maybe just limiting the animation to the transform property is enough
+            setTimeout(function() {
+                tray.addClass('anim');
+                tray.addClass('slide');
+            }, 30);
         });
         if ($.support.pjax) {
             $(document).on('click', 'a', function(event) {
