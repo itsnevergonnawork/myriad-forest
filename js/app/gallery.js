1915b79be6a716c0cd8fe8cd3ccfadea72542bce
Michał Koziarski <michalkoziarski@fastmail.fm>
2014-07-15 20:38:18 +0200

    Add Velocity.js, gallery arrows, switch images forwards and backwards

--- js/app/gallery.js
+++ js/app/gallery.js
@@ -1,30 +1,76 @@
 define([
-    'jquery'
+    'jquery',
+    'jquery.velocity'
 ], function($) {
     var init = function init() {
-        var content = $('.content');
-        var gallery = $('.gallery');
+        var content = $('#tray > .content');
+        var gallery = content.find('.gallery');
+        var galLeft = gallery.offset().left;
+        var galHalfWidth = gallery.width() / 2;
+        var side;
         var img = gallery.find('img');
         var photos = gallery.find('a');
         var count = photos.length;
         var idx = 0;
+        var prevArrow = gallery.find('.prev-image-arrow');
+        var nextArrow = gallery.find('.next-image-arrow');
+
+        var onResize = function onResize() {
+            galHalfWidth = gallery.width() / 2;
+        };
 
         if (!img.length)
             return;
 
         content.css('background-image', "url('" + img.attr('src') + "')");
 
-        gallery.click(function() {
+        gallery.click(function(event) {
             var imgSrc;
 
-            idx = (idx + 1) % count;
+            if (side) {
+                idx++;
+                console.log('next');
+            } else {
+                idx = idx + count - 1;
+                console.log('prev');
+            }
+            idx = idx % count;
             imgSrc = photos.eq(idx).attr('href');
             img.attr('src', imgSrc);
             content.css('background-image', "url('" + imgSrc + "')");
         });
+
+        gallery.mousemove(function(event) {
+            var left = event.pageX - galLeft;
+
+            if (left < galHalfWidth && (undefined == side || 1 == side)) {
+                side = 0;
+                nextArrow
+                    .velocity('stop')
+                    .velocity({ opacity: 0 }, { display: 'none' });
+                prevArrow
+                    .velocity('stop')
+                    .velocity({ opacity: 1 }, { duration: 50, display: 'block' });
+            }
+            if (left >= galHalfWidth && (undefined == side || 0 == side)) {
+                side = 1;
+                prevArrow
+                    .velocity('stop')
+                    .velocity({ opacity: 0 }, { display: 'none' });
+                nextArrow
+                    .velocity('stop')
+                    .velocity({ opacity: 1 }, { duration: 50, display: 'block' });
+            }
+        });
+
+        $(window).off('resize', onResize);
+        $(window).on('resize', onResize);
     };
 
     return {
-        init: init
+        init: function() {
+            if ($('.gallery').length)
+                init();
+        }
     };
 });
