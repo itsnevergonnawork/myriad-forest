81dda5be451503eb8e060b18b35327e60fa370b6
Michał Koziarski <michalkoziarski@fastmail.fm>
2014-08-03 01:28:52 +0200

    Fix sliding direction and show correct nav links
    Pages now slide to the left or to the right depending on which page
    we're going to. Works also for browser back/forward navigation.
    When going to a new page, nav links are updated accordingly.

--- js/app/navigation.js
+++ js/app/navigation.js
@@ -4,13 +4,36 @@ define([
     'app/pageSetup',
     'jquery.velocity'
 ], function($, pjax, pageSetup) {
-    var tray, incoming;
+    var tray, incoming,
+        prevLink, nextLink;
 
     var initDom = function initDom() {
         tray = $('#tray');
         incoming = $('<div class="incoming"></div>');
+        prevLink = $('.prevLink');
+        nextLink = $('.nextLink');
+        incoming.append($('.prev-next-page').clone());
         incoming.append($('.content').clone());
         tray.append(incoming);
+    };
+
+    var updatePrevNextLinks = function updatePrevNextLinks(linkData) {
+        var both = prevLink.add(nextLink);
+
+        both.removeAttr('href');
+        both.velocity({ opacity: 0 }, {
+            duration: 250,
+            complete: function() {
+                prevLink.text(linkData.prevPage.title);
+                nextLink.text(linkData.nextPage.title);
+            }
+        }).velocity({ opacity: 1 }, {
+            duration: 250,
+            complete: function() {
+                prevLink.attr('href', linkData.prevPage.path);
+                nextLink.attr('href', linkData.nextPage.path);
+            }
+        });
     };
 
     var init = function init() {
@@ -20,6 +43,10 @@ define([
             var incoming = $(this),
                 newPage = incoming.find('.content').clone(),
                 oldPage = tray.children('.content'),
+                prevNextBefore = tray.children('.prev-next-page'),
+                prevNextNow = incoming.find('.prev-next-page'),
+                prevNextBeforeObj,
+                prevNextNowObj,
                 moveRight = false,
                 propertyMap = {};
 
@@ -38,12 +65,27 @@ define([
                 pageSetup.setupPage();
             };
 
-            if ($(event.relatedTarget).hasClass('prevLink'))
-                moveRight = true;
+            try {
+                prevNextBeforeObj = JSON.parse(prevNextBefore.text());
+                prevNextNowObj = JSON.parse(prevNextNow.text());
+                if (prevNextBeforeObj.nextPage.path == prevNextNowObj.prevPage.path) {
+                    moveRight = true;
+                }
+            } catch (e) {
+                console.log('prev/next page data parse error');
+            }
+            prevNextBefore.text(prevNextNow.text());
+
+            /*if ($(event.relatedTarget).hasClass('prevLink'))
+                moveRight = true;*/
 
             newPage.addClass('new');
             tray.addClass('expanded' + (moveRight ? ' left' : ''));
             tray.append(newPage);
+
+            if (prevNextNowObj.prevPage) {
+                updatePrevNextLinks(prevNextNowObj);
+            }
 
             if (moveRight) {
                 propertyMap.translateX = ['50%', 0];
@@ -55,6 +97,7 @@ define([
                 complete: completeTrans
             });
         });
+
         if ($.support.pjax) {
             $(document).on('click', 'a', function(event) {
                 if (this.host == window.location.host) {
