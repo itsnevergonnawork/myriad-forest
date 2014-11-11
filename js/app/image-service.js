9eeb1db2957bf56e2a35b7b524d7c179cadc50a0
Michał Koziarski <michalkoziarski@fastmail.fm>
2014-11-11 15:41:07 +0100

    Load different size gallery images based on screen width

--- js/app/image-service.js
+++ js/app/image-service.js
@@ -1,7 +1,13 @@
 define([
-    'imagesloaded.pkgd'
-], function(imagesloaded) {
-    var galleries = [];
+    'imagesloaded.pkgd',
+    'enquire'
+], function(imagesloaded, enquire) {
+    var galleries = [],
+        basePaths = [
+            'assets/images-640/',
+            'assets/images/'
+        ],
+        basePath = basePaths[0];
 
     var getGalleryData = function getGalleryData(galleryId) {
         if (!galleries[galleryId]) {
@@ -12,8 +18,9 @@ define([
     };
 
     var requestImage = function getImagePath(galleryId, imgIdx, callback) {
+        // TODO try reusing a single image object for each new image file
         var imgNode = document.createElement('img'),
-            path = getGalleryData(galleryId)[imgIdx].path,
+            path = basePath + getGalleryData(galleryId)[imgIdx].path,
             imgLoad;
 
         imgNode.src = path;
@@ -28,6 +35,17 @@ define([
 
         return (index + 1) % length;
     };
+
+    var setupMediaQueries = function setupMediaQueries() {
+        enquire.register('screen and (max-width: 767px)', function() {
+            basePath = basePaths[0];
+        });
+        enquire.register('screen and (min-width: 768px)', function() {
+            basePath = basePaths[1];
+        });
+    }
+
+    setupMediaQueries();
 
     return {
         requestImage: requestImage,
